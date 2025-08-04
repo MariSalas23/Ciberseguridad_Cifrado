2cb6618154c1861ad16c566f21586da416f9c60b
MariSalas23 <mari.masagu@gmail.com>
2025-08-03 21:10:59 -0500

    Menu

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 from cifrado.cesar import cifrado_cesar, descifrado_cesar
 from cifrado.vigenere import cifrado_vigenere, descifrado_vigenere
 from ataques.ataques import ataque_fuerza_bruta_cesar, ataque_diccionario_vigenere
@@ -7,50 +6,70 @@ from clave.clave import pedir_clave_cesar, pedir_clave_vigenere
 def menu():
     while True:
         print("\nMENÚ:\n")
-        print("1. Cifrado César")
-        print("2. Descifrado César")
-        print("3. Cifrado Vigenère")
-        print("4. Descifrado Vigenère")
+        print("1. Cifrado")
+        print("2. Descifrado")
         print("0. Salir")
         opcion = input("\nSeleccione una opción: ")
         print("")
 
         if opcion == "1":
-            texto = input("Mensaje a cifrar: ")
-            clave = pedir_clave_cesar()
-            print("\nResultado:", cifrado_cesar(texto, clave))
+            print("\n---- CIFRADO ----")
+            print("1. Cifrado César")
+            print("2. Cifrado Vigenère")
+            subopcion = input("\nSeleccione tipo de cifrado: ")
+
+            if subopcion == "1":
+                texto = input("\nMensaje a cifrar: ")
+                clave = pedir_clave_cesar()
+                print("\nResultado:", cifrado_cesar(texto, clave))
+
+            elif subopcion == "2":
+                texto = input("\nMensaje a cifrar: ")
+                clave = pedir_clave_vigenere()
+                print("\nResultado:", cifrado_vigenere(texto, clave))
+
+            else:
+                print("\nOpción inválida. Intente de nuevo.")
 
         elif opcion == "2":
-            texto = input("Mensaje cifrado: ")
-            tiene_clave = input("¿Tiene la clave? (s/n): ").strip().lower()
-            if tiene_clave == "s":
-                clave = pedir_clave_cesar()
-                print("\nResultado:", descifrado_cesar(texto, clave))
-            else:
-                print("\nIniciando ataque por fuerza bruta...")
-                ataque_fuerza_bruta_cesar(texto)
+            print("\n----- DESCRIFRADO -----")
+            print("1. Descifrado César")
+            print("2. Descifrado Vigenère")
+            subopcion = input("\nSeleccione tipo de descifrado: ")
 
-        elif opcion == "3":
-            texto = input("Mensaje a cifrar: ")
-            clave = pedir_clave_vigenere()
-            print("\nResultado:", cifrado_vigenere(texto, clave))
+            if subopcion == "1":
+                texto = input("\nMensaje cifrado: ")
+                tiene_clave = input("¿Tiene la clave? (s/n): ").strip()
+                if tiene_clave == "s":
+                    clave = pedir_clave_cesar()
+                    print("\nResultado:", descifrado_cesar(texto, clave))
+                elif tiene_clave == "n":
+                    print("\nIniciando ataque por fuerza bruta...")
+                    ataque_fuerza_bruta_cesar(texto)
+                else:
+                    print("\nOpción inválida. Intenta de nuevo.")
 
-        elif opcion == "4":
-            texto = input("Mensaje cifrado: ")
-            tiene_clave = input("¿Tiene la clave? (s/n): ").strip().lower()
-            if tiene_clave == "s":
-                clave = pedir_clave_vigenere()
-                print("\nResultado:", descifrado_vigenere(texto, clave))
+            elif subopcion == "2":
+                texto = input("\nMensaje cifrado: ")
+                tiene_clave = input("¿Tiene la clave? (s/n): ").strip()
+                if tiene_clave == "s":
+                    clave = pedir_clave_vigenere()
+                    print("\nResultado:", descifrado_vigenere(texto, clave))
+                elif tiene_clave == "n":
+                    print("\nIniciando ataque con diccionario...")
+                    ataque_diccionario_vigenere(texto)
+                else:
+                    print("\nOpción inválida. Intenta de nuevo.")
+
             else:
-                print("\nIniciando ataque con diccionario...")
-                ataque_diccionario_vigenere(texto)
+                print("\nOpción inválida. Intenta de nuevo.")
 
         elif opcion == "0":
             print("\n¡Gracias por escoger a Firewallín!")
             break
 
         else:
-            print("\nOpción inválida. Intente de nuevo.")
+            print("\nOpción inválida. Intenta de nuevo.")
 
 if __name__ == "__main__":
     menu()
---
+++ recursos/rockyou_top100.py
@@ -0,0 +1,15 @@
+passwords = [
+    "password", "iloveyou", "princess", "rockyou", "nicole", "daniel", "babygirl", "monkey",
+    "lovely", "jessica", "michael", "ashley", "qwerty", "iloveu", "michelle", "tigger", "sunshine",
+    "chocolate", "soccer", "anthony", "friends", "butterfly", "purple", "angel", "jordan",
+    "liverpool", "justin", "loveme", "fuckyou", "football", "secret", "andrea", "carlos",
+    "jennifer", "joshua", "bubbles", "superman", "hannah", "amanda", "loveyou", "pretty",
+    "basketball", "andrew", "angels", "tweety", "flower", "playboy", "hello", "elizabeth",
+    "hottie", "tinkerbell", "charlie", "samantha", "barbie", "chelsea", "lovers", "teamo",
+    "jasmine", "brandon", "shadow", "melissa", "eminem", "matthew", "robert", "danielle",
+    "forever", "family", "jonathan", "computer", "whatever", "dragon", "vanessa", "cookie",
+    "naruto", "summer", "sweety", "spongebob", "joseph", "junior", "softball", "taylor",
+    "yellow", "daniela", "lauren", "mickey", "princesa", "banana", "orange", "sunrise", "lovebug",
+    "kitten", "peanut", "midnight", "ocean", "raspberry", "peaches", "rainbow", "buttercup",
+    "strawberry", "blueberry"
+]
